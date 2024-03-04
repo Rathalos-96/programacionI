e6817573175da404bf98d2ebab73c26bc24ea37e
joan <jsvelasquezc@uqvirtual.edu.co>
2024-03-03 22:15:07 -0500

    Ultimo cambio

---
+++ Ejercicio II/Veterinaria/veterinaria/src/main/java/co/edu/uniquindio/poo/Veterinaria.java
@@ -0,0 +1,49 @@
+package co.edu.uniquindio.poo;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class Veterinaria {
+    
+    private String nombre;
+    private Collection<Mascota> mascotas;
+
+    public Veterinaria(String nombre) {
+        this.nombre = nombre;
+        mascotas = new LinkedList<Mascota>();
+        assert nombre != null;
+    }
+
+    public void agregarMascotas(Mascota mascota)
+    {   
+        assert validarIdentificacionDuenoExistente(mascota.getIdentificacionDueno())==false;
+        mascotas.add(mascota);
+    }
+    public boolean validarIdentificacionDuenoExistente(String identificacionDueno)
+    {
+        for (Mascota mascota : mascotas) {
+            if (mascota.getIdentificacionDueno().equalsIgnoreCase(identificacionDueno)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Collection<Mascota> getMascotas() {
+        return mascotas;
+    }
+
+    public void setMascotas(Collection<Mascota> mascotas) {
+        this.mascotas = mascotas;
+    }
+    
+
+}
